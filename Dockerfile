cb2f7720baaf1275707322bbfb950d46b152034b
markocrnic <markons996@gmail.com>
2019-10-09 15:52:25 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.6-alpine as base
+
+FROM base as builder
+
+RUN mkdir /install
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+WORKDIR /install
+COPY requirements.txt /requirements.txt
+RUN pip install --install-option="--prefix=/install" -r /requirements.txt
+
+FROM base
+
+COPY --from=builder /install /usr/local
+COPY . /app
+RUN apk --no-cache add libpq
+WORKDIR /app
+EXPOSE 5000
+CMD ["python", "app/interface.py"]
---
+++ app/interface.py
@@ -0,0 +1,104 @@
+from flask import Flask, request
+from flask_cors import CORS
+from passlib.hash import sha256_crypt
+from schema import Schema, And, Use
+import json
+import requests
+import jwt
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+user_with_username_get_request = 'http://10.0.200.68:5003/api/users/'
+register_new_user = 'http://10.0.200.68:5003/api/users/'
+headers = {'content-type': 'application/json'}
+secret = 'planthealthcare'
+
+schema = Schema({'first_name': And(str, len),
+                 'last_name': And(str, len),
+                 'username': And(str, len),
+                 'email': And(str, len),
+                 'admin': And(Use(bool)),
+                 'password': And(str, len)})
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    try:
+        username = request.json['username']
+        password = request.json['password']
+
+        get_user = requests.get(user_with_username_get_request + username)
+
+        status = get_user.status_code
+
+        if status == 204:
+            return {'msg': 'User with that username does not exist.'}, 401
+        elif status != 200:
+            return {'msg': 'User-management API is not available.'}, 500
+
+        data = get_user.json()
+
+        if sha256_crypt.verify(password, data['password']):
+            print('Passwords match!')
+            encoded_jwt = jwt.encode(data, secret, algorithm='HS256')
+
+            response = create_response(data)
+
+            return {'user': response, 'token': str(encoded_jwt)}
+
+        else:
+            print('Passwords do not match!')
+            return {'msg': 'Authorization error. Credentials do not match!'}, 401
+
+    except Exception as e:
+        print(e)
+        return {"msg": "Something went wrong while authenticating user."}, 500
+
+
+@app.route('/register/', methods=['POST'])
+def register():
+    try:
+        try:
+            validated = schema.validate(request.json)
+        except Exception as e:
+            return {'msg': 'Data is not valid.'}, 403
+
+        validated_data = request.json
+
+        get_user = requests.get(user_with_username_get_request + validated_data['username'])
+        status = get_user.status_code
+
+        if status == 200:
+            return {'msg': 'User with that username already exist. Please choose another one.'}, 422
+
+        send_user = requests.post(register_new_user, headers=headers, data=json.dumps(validated_data))
+
+        status_code = send_user.status_code
+        if status_code == 201:
+            return {'msg': 'User registered successfully'}, 201
+        else:
+            return {'msg': 'User registration failed successfully'}, 500
+
+    except Exception as e:
+        print(e)
+        return {"msg": "Something went wrong while registering user."}, 500
+
+
+def create_response(data):
+    new_data = {}
+    new_data['first_name'] = data['first_name']
+    new_data['last_name'] = data['last_name']
+    new_data['username'] = data['username']
+    new_data['email'] = data['email']
+    new_data['admin'] = data['admin']
+
+    return new_data
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
