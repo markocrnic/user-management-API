9021132d828d2054a78bacb733a405d5a660f40b
markocrnic <markons996@gmail.com>
2019-11-05 16:02:59 +0100

    Added Jaeger tracing to all API's

--- app/interface.py
+++ app/interface.py
@@ -1,25 +1,42 @@
 from flask import Flask, request
 from flask_cors import CORS
+from api_management.jaeger import initializejaeger
+from flask_opentracing import FlaskTracing
 import implementation as implementation
 
 
 app = Flask(__name__)
 CORS(app)
 
+jaeger_tracer = initializejaeger()
+tracing = FlaskTracing(jaeger_tracer)
+
 
 @app.route('/usermanagement/login/', methods=['POST'])
+@tracing.trace()
 def login():
-    return implementation.checkLogin(request, False)
+    with jaeger_tracer.start_active_span(
+            'User-management-API endpoint /usermanagement/login/') as scope:
+        scope.span.log_kv({'event': 'Calling endpoint /usermanagement/login/', 'request_method': request.method})
+        return implementation.checkLogin(request, False)
 
 
 @app.route('/usermanagement/adminlogin/', methods=['POST'])
+@tracing.trace()
 def admin_login():
-    return implementation.checkLogin(request, True)
+    with jaeger_tracer.start_active_span(
+            'User-management-API endpoint /usermanagement/adminlogin/') as scope:
+        scope.span.log_kv({'event': 'Calling endpoint /usermanagement/adminlogin/', 'request_method': request.method})
+        return implementation.checkLogin(request, True)
 
 
 @app.route('/usermanagement/register/', methods=['POST'])
+@tracing.trace()
 def register():
-    return implementation.checkRegister(request)
+    with jaeger_tracer.start_active_span(
+            'User-management-API endpoint /usermanagement/register/') as scope:
+        scope.span.log_kv({'event': 'Calling endpoint /usermanagement/register/', 'request_method': request.method})
+        return implementation.checkRegister(request)
 
 
 if __name__ == '__main__':
