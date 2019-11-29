94d76df59b9831025e6b82dd866502eed6ab34e2
Marko Crnic <marko.crnic@devoteam.com>
2019-11-29 14:40:01 +0100

    Changed provisioning of api routes from FTP to ENV variables.

--- app/api_management/api_management.py
+++ app/api_management/api_management.py
@@ -1,40 +1,26 @@
-import ftputil
 from ast import literal_eval
 from loadconfig import load_config
-import os
 
 
 config = load_config('config/config.yml')
 #config = load_config('../config/config.yml')
 
 
-def readpathsfromftp():
+def getpaths():
     try:
-        a_host = ftputil.FTPHost(config['ftp']['host'], config['ftp']['user'], config['ftp']['password'])
+        content = config['api']['paths']
+        PATH_DICT = literal_eval(content)
 
-        for (dirname, subdirs, files) in a_host.walk(config['ftp']['path']):
-            for f in files:
-                if f == 'paths.txt':
-                    a_host.download(dirname + f, f)
-                    os.chmod('paths.txt', 0o777)
-                    with open(f) as txtfile:
-                        content = txtfile.read()
-                        print(str(content))
-        a_host.close()
-
-        return literal_eval(content)
-
+        return PATH_DICT
     except Exception as e:
         print(e)
-        a_host.close()
 
 
 def getpath(path):
-    path_dict = readpathsfromftp()
+    path_dict = getpaths()
     apiname = path.split('/')[0]
     if apiname in path_dict:
         apipath = path_dict[apiname] + path
-        print(apipath)
         return apipath
     else:
         print('Path not found: ' + path)
